dc7aa55386c7952fd8ca25fd95ed97c5f13d5c47
Leonid <qwertyleo2121@gmail.com>
2021-09-24 23:45:29 +0300

    Добавьте файлы проекта.

---
+++ HW_BubbleSortLIb/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HWbars;
+namespace HW_BubbleSortLIb
+{
+    class Program
+    {
+        static void Print (params int [] allparams)
+        {
+            foreach(int param in allparams)
+            {
+                Console.Write(param.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 2,10,25,7,11,22,9,5,4,3};
+            Print(array);
+            BubbleSortLib.sort(array);
+            Print(array);
+
+        }
+    }
+}
